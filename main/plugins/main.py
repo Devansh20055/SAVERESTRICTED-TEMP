3a040362afd4b3e04b038ac899aa40705f1db08f
DEVANSH <79394737+Devansh20055@users.noreply.github.com>
2022-02-23 12:10:55 +0530

    Update main.py

--- main/plugins/main.py
+++ main/plugins/main.py
@@ -14,6 +14,9 @@ from ethon.telefunc import fast_upload, fast_download, force_sub
 
 from main.plugins.helpers import get_link, join, screenshot
 
+
+ft = f"To use this bot you've to join @{fs}."
+
 @Drone.on(events.NewMessage(incoming=True, func=lambda e: e.is_private))
 async def clone(event):
     try:
